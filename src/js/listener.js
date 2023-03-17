2e7dde78c727b60c02673e0b9f8a3b8ca8f62fd9
dxt99 <ericlouis19x@gmail.com>
2023-03-17 16:34:03 +0700

    refactor: moved listeners to listener.js

--- src/js/listener.js
+++ src/js/listener.js
@@ -2,9 +2,40 @@ function addListener() {
 	// declare controls
 	const modelControl = document.getElementById("model");
 	const projectionControl = document.getElementById("projection");
+	const addRadius = document.getElementById("addRadius");
+	const reduceRadius = document.getElementById("reduceRadius");
+	const cameraAxis = document.getElementById("cameraAxis");
+	const cameraRotation = document.getElementById("cameraRotation");
 
 	projectionControl.addEventListener("change", function() {
 		console.log("projection changed");
 		model = modelControl.value;
 	});
+
+	function changeRadius(delta){
+		let curRadius = radius;
+		if (curRadius + delta <= 0.0001 || curRadius + delta >=0.999)return;
+		radius += delta;
+	}
+	
+	function setCameraSlider(){
+		if (cameraAxis.value == "Y"){
+			cameraRotation.value = yAxis;
+		} else{
+			cameraRotation.value = xAxis;
+		}
+	}
+	
+	function updateCameraRotation(){
+		if (cameraAxis.value == "Y"){
+			yAxis = cameraRotation.value;
+		} else{
+			xAxis = cameraRotation.value;
+		}
+	}
+	
+	addRadius.onclick = () => changeRadius(0.05);
+	reduceRadius.onclick = () => changeRadius(-0.05);
+	cameraAxis.onchange = () => setCameraSlider();
+	cameraRotation.oninput = () => updateCameraRotation();
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,6 @@
 // constants
 const canvas = document.getElementById("gl-canvas");
 const gl = canvas.getContext("webgl");
-const addRadius = document.getElementById("addRadius");
-const reduceRadius = document.getElementById("reduceRadius");
-const cameraAxis = document.getElementById("cameraAxis");
-const cameraRotation = document.getElementById("cameraRotation");
 
 /* program states */
 var models = [] // models has model objects that has array of vec4 points and colors
@@ -158,30 +154,5 @@ function render(){
 	window.requestAnimationFrame(render)
 }
 
-function changeRadius(delta){
-	let curRadius = radius;
-	if (curRadius + delta <= 0.0001 || curRadius + delta >=0.999)return;
-	radius += delta;
-}
-
-function setCameraSlider(){
-	if (cameraAxis.value == "Y"){
-		cameraRotation.value = yAxis;
-	} else{
-		cameraRotation.value = xAxis;
-	}
-}
-
-function updateCameraRotation(){
-	if (cameraAxis.value == "Y"){
-		yAxis = cameraRotation.value;
-	} else{
-		xAxis = cameraRotation.value;
-	}
-}
-
-addRadius.onclick = () => changeRadius(0.05);
-reduceRadius.onclick = () => changeRadius(-0.05);
-cameraAxis.onchange = () => setCameraSlider();
-cameraRotation.oninput = () => updateCameraRotation();
 main();
+addListener();
